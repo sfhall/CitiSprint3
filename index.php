2b60e650ad6b77f5bd3f1f6a9f2b9168627d84ff
Sarah Hall <35583736+sfhall@users.noreply.github.com>
2020-07-28 10:15:56 -0400

    init commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Citi Budget Portal</title>
+</head>
+
+<body>
+    <div class="container">
+<!--splash-->
+        <div id="splash">
+            <div class="logo">
+                <img src="citibank.png">
+            </div>
+
+            <div class="tabs">
+                <p>tabs</p>
+            </div>
+
+            <div class="menu">
+                <p>hamburger</p>
+            </div>
+        </div>
+<!--content-->
+        <div class="power">
+            <p>
+            <iframe width="1540" height="750" src="https://app.powerbi.com/reportEmbed?reportId=5ad12acc-af86-4860-9566-6591d28b0805&autoAuth=true&ctid=dc046083-4ce8-437a-bd49-e995b65e4d11&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXVzLWVhc3QyLXJlZGlyZWN0LmFuYWx5c2lzLndpbmRvd3MubmV0LyJ9" frameborder="0" allowFullScreen="true"></iframe>
+            </p>
+        </div>
+
+        <div class="buttons">
+            <button type="button">Input Data</button> 
+            <button type="button">Create Budget</button> 
+            <button type="button">Edit Budget</button> 
+        </div>
+
+        <div class="requests">
+            <p>requests</p>
+        </div>
+
+
+</body>
+</html>
